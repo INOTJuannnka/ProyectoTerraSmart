5bc069e7a1ca649d6470584ec61cf0c44069a1a3
INOTJuannnka <jucarvajal@unicauca.edu.co>
2025-05-04 16:09:23 -0500

    commit -m "fix: fix templates path"

--- TerraSmart/views/views.py
+++ TerraSmart/views/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
 # Create your views here.
 #index
-def HolaMundo(request):
 
-    #return render(request, 'index.html')
-    
-    return HttpResponse("Hola Mundo desde Django!")
+def vista_inicio(request):
+    return render(request, 'inicio.html')
