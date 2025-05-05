9f526cf464c1258890bf00423d6810c32471e0fc
INOTJuannnka <jucarvajal@unicauca.edu.co>
2025-05-04 23:20:45 -0500

    commit -m feat: Conectividad con la base de datos

--- project_core/settings.py
+++ project_core/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'TerraSmart',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'project_core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS':  [os.path.join(BASE_DIR, 'templates')],
+        'DIRS':  [BASE_DIR / "TerraSmart/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
