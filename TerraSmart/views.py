d243f9b83300480d838e21a7d82ff59ff3248870
Miguel <miguelpolo9935gmail.com>
2025-05-04 19:16:15 -0500

    Nuevos templates

--- TerraSmart/views.py
+++ TerraSmart/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+def inicio(request):
+    return render(request, 'inicio.html')
+
+def mediciones(request):
+    return render(request, 'mediciones.html')
+
+def recomendaciones(request):
+    return render(request, 'recomendaciones.html')
+
+def configuracion(request):
+    return render(request, 'configuracion.html')
+
+def historial(request):
+    return render(request, 'historial.html')
