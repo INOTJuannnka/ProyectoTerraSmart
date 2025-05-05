a861f06fe13753140f5fcd627d4debe372b26c2c
INOTJuannnka <jucarvajal@unicauca.edu.co>
2025-05-04 23:20:45 -0500

    commit -m feat: Conectividad con la base de datos

--- TerraSmart/models.py
+++ TerraSmart/models.py
@@ -1,5 +1,31 @@
 from django.db import models
 
-# Create your models here.
-# Noches Buenas por la Mañana :D
-#aaaaaaaaaaaaa
+#class regisDatos(models.Model):
+#    nitrogeno = models.CharField(max_length=100, verbose_name='Nitrogeno')
+#    fosforo = models.CharField(max_length=100, verbose_name='Fosforo')
+#    potasio = models.CharField(max_length=100, verbose_name='Potasio')
+#    ph = models.CharField(max_length=100, verbose_name='ph')
+#    humedad = models.CharField(max_length=100, verbose_name='Humedad')
+
+class Medicion(models.Model):
+    idMedicion = models.AutoField(primary_key=True)
+    fecha = models.DateField(null=True, blank=True)
+    Nitrogeno = models.FloatField(null=True, blank=True)
+    Fosforo = models.FloatField(null=True, blank=True)
+    Medicioncol = models.CharField(max_length=255, null=True, blank=True)  # Ajusta según el tipo real
+    potasio = models.FloatField(null=True, blank=True)
+    ph = models.FloatField(null=True, blank=True)
+    humedad = models.FloatField(null=True, blank=True)
+    class Meta:
+        managed = False  
+        db_table = 'medicion'  
+
+class registroSuelo(models.Model):
+    nitrogeno = models.IntegerField(verbose_name='Nitrogeno')
+    fosforo = models.IntegerField(verbose_name='Fosforo')
+    potasio = models.IntegerField(verbose_name='Potasio')
+    ph = models.IntegerField(verbose_name='ph')
+    humedad = models.IntegerField(verbose_name='Humedad')
+
+    def __str__(self):
+        return f'{self.nitrogeno} {self.fosforo} {self.potasio} {self.ph} {self.humedad}'
--- TerraSmart/urls.py
+++ TerraSmart/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from TerraSmart.views.views import vista_inicio
+from TerraSmart.views.views import vista_inicio, vista_historial
 
 
 urlpatterns = [
     path('', vista_inicio, name='inicio'),
+    path('historial/', vista_historial, name='historial'),
 ]
