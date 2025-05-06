d2913234cf851d883ee44c70e65971b2dc0bcba4
INOTJuannnka <jucarvajal@unicauca.edu.co>
2025-05-06 00:27:26 -0500

    commit -m "feat: Se agrega la funcionalidad de mediciones"

--- TerraSmart/models.py
+++ TerraSmart/models.py
@@ -1,18 +1,10 @@
 from django.db import models
 
-#class regisDatos(models.Model):
-#    nitrogeno = models.CharField(max_length=100, verbose_name='Nitrogeno')
-#    fosforo = models.CharField(max_length=100, verbose_name='Fosforo')
-#    potasio = models.CharField(max_length=100, verbose_name='Potasio')
-#    ph = models.CharField(max_length=100, verbose_name='ph')
-#    humedad = models.CharField(max_length=100, verbose_name='Humedad')
-
 class Medicion(models.Model):
     idMedicion = models.AutoField(primary_key=True)
     fecha = models.DateField(null=True, blank=True)
-    Nitrogeno = models.FloatField(null=True, blank=True)
-    Fosforo = models.FloatField(null=True, blank=True)
-    Medicioncol = models.CharField(max_length=255, null=True, blank=True)  # Ajusta según el tipo real
+    nitrogeno = models.FloatField(null=True, blank=True)
+    fosforo = models.FloatField(null=True, blank=True)
     potasio = models.FloatField(null=True, blank=True)
     ph = models.FloatField(null=True, blank=True)
     humedad = models.FloatField(null=True, blank=True)
@@ -20,12 +12,19 @@ class Medicion(models.Model):
         managed = False  
         db_table = 'medicion'  
 
-class registroSuelo(models.Model):
-    nitrogeno = models.IntegerField(verbose_name='Nitrogeno')
-    fosforo = models.IntegerField(verbose_name='Fosforo')
-    potasio = models.IntegerField(verbose_name='Potasio')
-    ph = models.IntegerField(verbose_name='ph')
-    humedad = models.IntegerField(verbose_name='Humedad')
 
+class postMediciones(models.Model):
+    idMedicion = models.AutoField(primary_key=True)
+    fecha = models.DateField(null=True, blank=True)
+    nitrogeno = models.FloatField(null=True, blank=True)
+    fosforo = models.FloatField(null=True, blank=True)
+    potasio = models.FloatField(null=True, blank=True)
+    ph = models.FloatField(null=True, blank=True)
+    humedad = models.FloatField(null=True, blank=True)
+
+    class Meta:
+        managed = False  
+        db_table = 'medicion'
+    
     def __str__(self):
         return f'{self.nitrogeno} {self.fosforo} {self.potasio} {self.ph} {self.humedad}'
--- TerraSmart/urls.py
+++ TerraSmart/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from . import views
+from TerraSmart import views
 
 urlpatterns = [
-    path('', views.inicio, name='inicio'),
-    path('mediciones/', views.mediciones, name='mediciones'),
+    path('', views.vista_inicio, name='inicio'),
+    path('mediciones/', views.vista_mediciones, name='mediciones'),
     path('recomendaciones/', views.recomendaciones, name='recomendaciones'),
     path('configuracion/', views.configuracion, name='configuracion'),
-    path('historial/', views.historial, name='historial'),
+    path('historial/', views.vista_historial, name='historial'),
 ]
