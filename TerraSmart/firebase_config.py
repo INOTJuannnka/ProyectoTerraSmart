fd80038cc63cf217a8ce98568a0ab62924cb8f8f
INOTJuannnka <jucarvajal@unicauca.edu.co>
2025-05-19 15:10:46 -0500

    settings cambiado

--- TerraSmart/firebase_config.py
+++ TerraSmart/firebase_config.py
@@ -2,6 +2,6 @@
 import firebase_admin
 from firebase_admin import credentials,firestore
 
-cred = credentials.Certificate("C:\\Users\\jvuan\\Documents\\ProyectoTerraSmart\\TerraSmart\\terrasmart-c9c6d-firebase-adminsdk-fbsvc-c4df462741.json")
+cred = credentials.Certificate("C:/Users/user/Documents/2025.1/Proyecto/ProyectoTerraSmart2.0/ProyectoTerraSmart/TerraSmart/terrasmart-c9c6d-firebase-adminsdk-fbsvc-c4df462741.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
