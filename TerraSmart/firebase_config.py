8623ac8c5d3a61661dedece323e0101786e5f818
MiguelP0205 <miguelpolo9935@gmail.com>
2025-06-07 02:08:57 -0500

    Traducciones completas

--- TerraSmart/firebase_config.py
+++ TerraSmart/firebase_config.py
@@ -7,10 +7,10 @@ from firebase_admin import credentials,firestore
 #cred = credentials.Certificate("C:\\Users\\jvuan\\Documents\\TerraSmart\\ProyectoTerraSmart\\TerraSmart\\terrasmart-c9c6d-firebase-adminsdk-fbsvc-c4df462741.json")
 
 # Ubicacion Miguel PC
-#cred = credentials.Certificate("D:/Mis documentos/Downloads/TerraSmart/ProyectoTerraSmart/TerraSmart/terrasmart-c9c6d-firebase-adminsdk-fbsvc-c4df462741.json")
+cred = credentials.Certificate("D:/Mis documentos/Downloads/TerraSmart/ProyectoTerraSmart/TerraSmart/terrasmart-c9c6d-firebase-adminsdk-fbsvc-c4df462741.json")
 
 # Ubicacion PC Julian
-cred = credentials.Certificate("C:/Users/user/Documents/2025.1/Proyecto/ProyectoTerraSmart2.0/ProyectoTerraSmart/TerraSmart/terrasmart-c9c6d-firebase-adminsdk-fbsvc-c4df462741.json")
+#cred = credentials.Certificate("C:/Users/user/Documents/2025.1/Proyecto/ProyectoTerraSmart2.0/ProyectoTerraSmart/TerraSmart/terrasmart-c9c6d-firebase-adminsdk-fbsvc-c4df462741.json")
 
 firebase_admin.initialize_app(cred)
 db = firestore.client()
