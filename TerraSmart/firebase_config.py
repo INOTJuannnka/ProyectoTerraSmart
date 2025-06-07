04113d2ed2cecafd45f24b3b73f00de592852832
INOTJuannnka <jucarvajal@unicauca.edu.co>
2025-06-06 21:29:37 -0500

    Cambios en view

--- TerraSmart/firebase_config.py
+++ TerraSmart/firebase_config.py
@@ -3,6 +3,7 @@ import firebase_admin
 from firebase_admin import credentials,firestore
 
 # Ubicacion Juan PC
+#cred = credentials.Certificate("C:/Users/user/Documents/2025.1/Proyecto/ProyectoTerraSmart2.0/ProyectoTerraSmart/TerraSmart/terrasmart-c9c6d-firebase-adminsdk-fbsvc-c4df462741.json")
 #cred = credentials.Certificate("C:\\Users\\jvuan\\Documents\\TerraSmart\\ProyectoTerraSmart\\TerraSmart\\terrasmart-c9c6d-firebase-adminsdk-fbsvc-c4df462741.json")
 
 # Ubicacion Miguel PC
