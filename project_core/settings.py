6c29f160da4a7a009f8feedd16cd681712940472
INOTJuannnka <jucarvajal@unicauca.edu.co>
2025-06-09 15:11:48 -0500

    Correos al registrar

--- project_core/settings.py
+++ project_core/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
        'ENGINE': 'django.db.backends.mysql',
        'NAME': 'terrasmart',
        'USER': 'root',
-       'PASSWORD': 'mysql',
+       'PASSWORD': 'admin',
        'HOST': '127.0.0.1',
        'PORT': '3306', 
    }
