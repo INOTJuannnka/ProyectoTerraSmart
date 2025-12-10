94256dbf95ac36d641edb34b6f5b53bd164c64fb
Julian Carvajal <76658642+INOTJuannnka@users.noreply.github.com>
2025-12-10 18:24:36 -0500

    Update EMAIL_HOST_USER to a placeholder value

--- project_core/settings.py
+++ project_core/settings.py
@@ -181,7 +181,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'jucarvajal@unicauca.edu.co'
+EMAIL_HOST_USER = 'yourEmail'
 EMAIL_HOST_PASSWORD = 'aluf hgmn ottw fqfy'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
